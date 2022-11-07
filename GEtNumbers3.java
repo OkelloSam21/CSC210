21ee4155b0767d81e5c7be877de74a651969b569
OkelloSam <115361239+OkelloSam21@users.noreply.github.com>
2022-11-07 09:21:42 +0300

    Update GEtNumbers3.java

--- GEtNumbers3.java
+++ GEtNumbers3.java
@@ -6,26 +6,27 @@ import java.util.Scanner;
 
 public class GEtNumbers3 {
     public static void main(String[] args) {
-        Scanner stdin = new Scanner(System.in);
-        int firstNumber =0;
-        int secondNumber = 0;
-        boolean done = false;
-        while (!done) {
-                    try {
-                        System.out.print("\n\nEnter the first non-negative number : ");
-                        firstNumber = stdin.nextInt();
-                        System.out.print("Enter the second non-negative number : ");
-                        secondNumber = stdin.nextInt();
-                        done = true;
-            
-        } catch (InputMismatchException e) {
-            // TO DO: handle exception
-            stdin.nextLine();
-            System.out.println("!error...Input Mismatch!!");
-            System.out.println("Try again.");
+        try (Scanner stdin = new Scanner(System.in)) {
+            int firstNumber =0;
+            int secondNumber = 0;
+            boolean done = false;
+            while (!done) {
+                        try {
+                            System.out.print("\n\nEnter the first non-negative number : ");
+                            firstNumber = stdin.nextInt();
+                            System.out.print("Enter the second non-negative number : ");
+                            secondNumber = stdin.nextInt();
+                            done = true;
+                
+            } catch (InputMismatchException e) {
+                // TO DO: handle exception
+                stdin.nextLine();
+                System.out.println("!error...Input Mismatch!!");
+                System.out.println("Try again.");
+            }
+            }
+            System.out.println("Your numbers are " + firstNumber + " and " + secondNumber);
         }
-        }
-        System.out.println("Your numbers are " + firstNumber + " and " + secondNumber);
         
     }
 }
